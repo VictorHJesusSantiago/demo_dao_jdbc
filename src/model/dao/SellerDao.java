bbb722d2b1b021fdae1728224c01dca2cad1ecfd
Victor Santiago <victorhenriquedejesussantiago@gmail.com>
2024-04-07 09:12:32 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,7 +5,8 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-	void insert (Seller obj);
+
+	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -2,27 +2,23 @@ package model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
 
-public class Seller implements Serializable{
-	/**
-	 * 
-	 */
+public class Seller implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+
 	private Integer id;
 	private String name;
-	private String email; 
+	private String email;
 	private Date birthDate;
-	private double baseSalary;
-	
+	private Double baseSalary;
+
 	private Department department;
-	
+
 	public Seller() {
-		
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, double baseSalary, Department department) {
-		super();
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -63,11 +59,11 @@ public class Seller implements Serializable{
 		this.birthDate = birthDate;
 	}
 
-	public double getBaseSalary() {
+	public Double getBaseSalary() {
 		return baseSalary;
 	}
 
-	public void setBaseSalary(double baseSalary) {
+	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
 
@@ -81,7 +77,10 @@ public class Seller implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -93,7 +92,12 @@ public class Seller implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -101,5 +105,4 @@ public class Seller implements Serializable{
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
 }
